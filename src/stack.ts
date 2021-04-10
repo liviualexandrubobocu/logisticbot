905c507c9d416bee84dfded699fbe5664af687a9
Liviu Bobocu <liviualexandrubobocu@gmail.com>
2021-04-10 19:09:16 +0300

    Compositional stack algorithm

--- src/stack.ts
+++ src/stack.ts
@@ -1,20 +1,52 @@
 const MAX = 1000;
+
 type IStack = {
-    top: number;
-    a?: any[];
-}
+    top?: number | undefined;
+    a?: any[] | undefined;
+    push: (x: any) => boolean | undefined;
+} | undefined;
+
 function Stack(): IStack {
     return {
-        top: -1
+        top: -1,
+        a: [],
+        push: () => undefined
     }
 }
-const push = (stack: IStack, x: any) => {
-    let { a, top } = stack;
-    if(top >= MAX - 1 ){
+
+function push(x: any) {
+    if(this.top >= MAX - 1 ){
         console.log("Stack overflow");
         return false;
     } else {
-        a[++top] = x;
+        this.top++;
+        this.a[this.top] = x;
+        setStack(this);
         console.log(`pushed on the stack ${x}`);
+        return true;
     }
 }
+
+
+const setStack = (s?: IStack): IStack => {
+    let stack = s ? {...s} : Stack();
+    stack.push = push;
+    return stack;
+}
+
+function main() {
+    let stack1 = setStack();
+    stack1.push(3);
+    stack1.push(5);
+
+    
+    let stack2 = setStack();
+    stack2.push('a');
+    stack2.push('b');
+
+    
+    console.log(stack1);
+    console.log(stack2);
+
+}
+main();
